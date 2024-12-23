0df508d82b40fec825a486ab9a13909dd9789f8a
rutuja785 <rsyewale7@gmail.com>
2024-12-23 16:51:22 +0530

    add /mars endpoint

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,16 @@
-console.log("Hello World!\n");
+var express = require('express');
+app = express();
+
+app.get('/',function (req,res){
+    res.send('Hello World!\n');
+});
+
+app.get('/mars', function(req,res){
+    res.send('Hello Mars!\n');
+});
+
+app.listion(8080,function(){
+    console.log("Hello World");
+});
+
 
